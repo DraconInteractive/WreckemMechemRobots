73bd294f560c13073f53fb8d9c65348d8de53739
RarCeth <rarceth@gmail.com>
2020-02-02 15:11:08 +0800

    asdf

---
+++ Assets/OilCan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilCan : MonoBehaviour
+{
+    public Transform oilPoint;
+    public float oilDist;
+    public AudioSource source;
+    public void Oil ()
+    {
+        SqueakyBoi[] bois = GameObject.FindObjectsOfType<SqueakyBoi>();
+        List<Transform> toDestroy = new List<Transform>();
+        foreach (SqueakyBoi boi in bois)
+        {
+            if (Vector3.Distance(boi.transform.position, oilPoint.position) < oilDist)
+            {
+                AudioSource source = boi.GetComponent<AudioSource>();
+                if (source != null)
+                {
+                    Destroy(source);
+                }
+                toDestroy.Add(boi.transform);
+            }
+        }
+
+        foreach (Transform t in toDestroy)
+        {
+            Destroy(t.GetComponent<SqueakyBoi>());
+        }
+
+        source.Play();
+    }
+
+}
