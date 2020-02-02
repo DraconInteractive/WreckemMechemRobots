20d700ae01a5748e00432fa83e162675a656e1b5
SoapyFred <triplef.mulholland@gmail.com>
2020-02-02 14:54:16 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/RarCeth/WreckemMechemRobots""
    This reverts commit e2924d3b2af69e4809147301fadd908ab52011c8.

---
+++ Assets/Starer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starer : MonoBehaviour
+{
+    public void Grab ()
+    {
+        HeadController.Instance.starers++;
+    }
+
+    public void Release ()
+    {
+        HeadController.Instance.starers--;
+    }
+}
